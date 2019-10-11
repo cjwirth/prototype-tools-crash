09ed6409b738cc4475af1ecc0130ed79c6a2b9f7
Caesar Wirth <caesar@cjwirth.com>
2019-10-11 10:25:22 -0400

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Caesar Wirth on 10/11/19.
+//  Copyright © 2019 Caesar Wirth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
